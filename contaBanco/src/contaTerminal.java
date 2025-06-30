fc4b31ffe1b8e840757d03399de5b3a9d0ef77f6
Debora Ribeiro <debii.ribeiro@gmail.com>
2025-06-30 10:35:36 -0300

    Update contaTerminal.java

--- contaBanco/src/contaTerminal.java
+++ contaBanco/src/contaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta:");
+        int numeroConta = sc.nextInt();
+        System.out.println("Número da Agência: ");
+        String numeroAgencia = sc.next();
+        sc.nextLine(); 
+        System.out.println("Nome do cliente:");
+        String nomeCliente = sc.nextLine();
+        System.out.println("Digite o saldo da conta:");
+        double saldoConta = sc.nextDouble();
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por ter criado uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
+
+        sc.close();
     }
 }
